af31e090229623ec69023b7c1a7c1f8dee150c5b
LeahJKH <leahjb06.kodehode@gmail.com>
2024-09-05 13:14:43 +0200

    added images

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-// Add Express
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-// Initialize Express
 const app = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "public")));
 
 // Read JSON files
 const vocaloids = JSON.parse(fs.readFileSync(path.join(__dirname, "json/vocaloids.json"), "utf-8"));
 const mikuTimeLine = JSON.parse(fs.readFileSync(path.join(__dirname, "json/mikuTimeLine.json"), "utf-8"));
 const mikuFacts = JSON.parse(fs.readFileSync(path.join(__dirname, "json/mikuFacts.json"), "utf-8"));
 
-// Create GET request
+// Create GET requests
 app.get("/", (req, res) => {
-  res.send("up and running! welcome too miku api read docs on how too use. https://github.com/LeahJKH/MikuApiGithub");
+  res.send("up and running! Welcome to the Miku API. Read docs on how to use: https://github.com/LeahJKH/MikuApiGithub");
 });
+
 app.get("/vocaloids", (req, res) => {
   res.json(vocaloids);
 });
@@ -26,6 +28,7 @@ app.get("/mikuTimeLine", (req, res) => {
 app.get("/mikuFacts", (req, res) => {
   res.json(mikuFacts);
 });
+
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
