3d0f8a38a16f3d7c3bcfa233a77e77a678b9b99a
LeahJKH <leahjb06.kodehode@gmail.com>
2024-09-10 11:40:37 +0200

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors"); // Import the cors package
 
 const app = express();
+
+// Use CORS middleware
+app.use(cors());
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
@@ -17,6 +21,7 @@ app.get("/", (req, res) => {
   res.send("up and running! Welcome to the Miku API. Read docs on how to use: https://github.com/LeahJKH/MikuApiGithub");
 });
 
+// Get json files
 app.get("/vocaloids", (req, res) => {
   res.json(vocaloids);
 });
@@ -28,6 +33,7 @@ app.get("/mikuTimeLine", (req, res) => {
 app.get("/mikuFacts", (req, res) => {
   res.json(mikuFacts);
 });
+// Get json files
 
 // Initialize server
 app.listen(5000, () => {
